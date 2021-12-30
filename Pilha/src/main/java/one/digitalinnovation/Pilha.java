b285a513f4870ed18d1c441b006b097b0941cc83
luanalimadeveloper <luanalima.developer@gmail.com>
2021-12-29 23:03:12 -0300

    Implementation of Stack class

---
+++ Pilha/src/main/java/one/digitalinnovation/Pilha.java
@@ -0,0 +1,41 @@
+package one.digitalinnovation;
+
+public class Pilha {
+
+    private No refNoEntradaPilha;
+
+    public Pilha() {
+        this.refNoEntradaPilha = null;
+    }
+
+    public void push(No novoNo){
+        No refAuxiliar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setRefNo(refAuxiliar); // nó que está embaixo é a antiga referencia de topo
+    }
+
+    public No pop(){
+        if(!this.isEmpty()){
+            No noPoped = refNoEntradaPilha;
+            refNoEntradaPilha = refNoEntradaPilha.getRefNo();
+            return noPoped;
+        }
+        return null;
+    }
+
+    public No top() {
+        return refNoEntradaPilha;
+    }
+
+    public boolean isEmpty(){
+ /*      if(refNoEntradaPilha == null){
+           return true;
+             }
+             return false;
+  */
+        return refNoEntradaPilha == null ? true : false;
+    }
+
+
+
+}
