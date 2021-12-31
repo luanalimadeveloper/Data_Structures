f2f509051d8fa7eca3269656b2d90ea622489c62
luanalimadeveloper <luanalima.developer@gmail.com>
2021-12-31 00:01:52 -0300

    Add comments in The Queue project

--- Fila/src/main/java/one/digitalinnovation/Main.java
+++ Fila/src/main/java/one/digitalinnovation/Main.java
@@ -6,18 +6,22 @@ public class Main {
 
         Fila minhaFila = new Fila();
 
+        // make the Queue
         minhaFila.enqueue(new No("primeiro"));
         minhaFila.enqueue(new No("segundo"));
         minhaFila.enqueue(new No("terceiro"));
         minhaFila.enqueue(new No("quarto"));
         System.out.println(minhaFila);
 
+        // The first element go out
         System.out.println(minhaFila.dequeue());
         System.out.println(minhaFila);
 
+        // add new element
         minhaFila.enqueue(new No("ultimo"));
         System.out.println(minhaFila);
 
+        // to see the first
         System.out.println(minhaFila.first());
         System.out.println(minhaFila);
 
